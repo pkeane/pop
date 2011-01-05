121274319dfb772d229f09024d971be6a68ad44e
pkeane <pkeane@mail.utexas.edu>
2011-01-05 17:07:22 -0600

    switched to Smarty3 from Dwoo

--- lib/Pop/Template.php
+++ lib/Pop/Template.php
@@ -4,24 +4,15 @@ class Pop_Template {
 
 		public function __construct($request)
 		{
-				require_once 'dwoo/dwooAutoload.php';
-				$this->dwoo = new Dwoo(TEMPLATE_COMPILE_PATH);
-				$this->data = new Dwoo_Data();
-				$this->data->assign('main_title', MAIN_TITLE);
-				$this->data->assign('request', $request);
-				$this->data->assign('msg', $request->get('msg'));
-				$this->data->assign('app_root', trim($request->app_root,'/').'/');
+				require_once 'smarty/libs/Smarty.class.php';
+				$this->smarty = new Smarty();
+        $this->smarty->template_dir = trim(TEMPLATE_PATH,'/').'/';
+        $this->smarty->compile_dir = trim(TEMPLATE_COMPILE_PATH,'/').'/';
+				$this->smarty->assign('main_title', MAIN_TITLE);
+				$this->smarty->assign('request', $request);
+				$this->smarty->assign('msg', $request->get('msg'));
+				$this->smarty->assign('app_root', trim($request->app_root,'/').'/');
 				$this->template_path = TEMPLATE_PATH;
 		}
 
-		public function assign($key,$val)
-		{
-				$this->data->assign($key,$val);
-		}
-
-		public function fetch($resource_name)
-		{
-				$tpl = new Dwoo_Template_File($this->template_path.'/'.$resource_name);
-				return $this->dwoo->get($tpl,$this->data);
-		}
 }
