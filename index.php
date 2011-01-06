1e0129eb62ebf034d80f3b637ef3d39d86c4d092
pkeane <pkeane@mail.utexas.edu>
2011-01-06 15:11:59 -0600

    added bootstrap file

--- index.php
+++ index.php
@@ -24,8 +24,6 @@ THE SOFTWARE.
 
 ***/
 
-//apc_clear_cache();
-
 //PHP ERROR REPORTING -- turn off for production
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
@@ -33,20 +31,7 @@ error_reporting(E_ALL);
 
 define('BASE_PATH',dirname(__FILE__));
 
-ini_set('include_path',BASE_PATH.'/lib');
-
-function popAutoload($class_name) {
-	$class_name = str_replace('_','/',$class_name).'.php';
-	@include ($class_name);
-}
-
-spl_autoload_register('popAutoload');
-
-define('HANDLER_PATH',BASE_PATH.'/handlers');
-define('TEMPLATE_PATH',BASE_PATH.'/templates');
-define('TEMPLATE_COMPILE_PATH',BASE_PATH.'/templates_c');
-define('DEFAULT_HANDLER','sample');
-define('MAIN_TITLE','my app');
+include BASE_PATH.'/inc/bootstrap.php';
 
 $r = new Pop_Http_Request();
 $r->getHandlerObject()->dispatch($r);
